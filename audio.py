c32fbe494885b16f2e4d6c38cb09e25bb3999e67
Mouzone <sunnyliu010@gmail.com>
2024-08-21 18:09:21 -0400

    Add .wav files to .gitignore and write function to record audio

---
+++ audio.py
@@ -0,0 +1,40 @@
+# 1) grab audio and play it back
+import pyaudio
+import wave
+
+
+def record_audio():
+    filename = "recorded.wav"
+    chunk = 1024
+    FORMAT = pyaudio.paInt16
+    channels = 1
+    sample_rate = 44100
+    record_seconds = 5
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=channels,
+                    rate=sample_rate,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=chunk)
+    frames = []
+    print("Recording...")
+    for i in range(int(sample_rate / chunk * record_seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+    print("Finished recording.")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    wf = wave.open(filename, "wb")
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(sample_rate)
+    wf.writeframes(b"".join(frames))
+    wf.close()
+
+
+record_audio()
+
+
+# 2) parse audio into words
