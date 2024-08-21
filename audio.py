1d57d4aa8aebb7d12e0b4f7558aacd8d2654a8b1
Mouzone <sunnyliu010@gmail.com>
2024-08-21 19:37:33 -0400

    Refactor audio into separate functions

--- audio.py
+++ audio.py
@@ -1,20 +1,24 @@
 import speech_recognition as sr
 
-r = sr.Recognizer()
-# rewrite to record with start and stop
-# while started, record for 5 second increments and parse
-with sr.Microphone() as source:
-    print("Listening...")
-    # Adjust for ambient noise and record the audio
-    r.adjust_for_ambient_noise(source)
-    audio_data = r.record(source, duration=5)
 
-    print("Recognizing...")
+def init():
+    r = sr.Recognizer()
+    r.adjust_for_ambient_noise(sr.Microphone)
+    return r
+
+
+def recordAudio(r):
+    # Adjust for ambient noise and record the audio
+    audio_data = r.record(sr.Microphone, duration=5)
+    return audio_data
+
+
+def parseAudio(r, audio_data):
     try:
         # Convert speech to text using Google's speech recognition API
         text = r.recognize_google(audio_data)
-        print("You said: " + text)
+        return text
     except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand the audio")
+        return "ERROR: Google Speech Recognition could not understand the audio"
     except sr.RequestError as e:
-        print(f"Could not request results from Google Speech Recognition service; {e}")
+        return f"ERROR: Could not request results from Google Speech Recognition service; {e}"
