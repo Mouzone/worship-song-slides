bdd0ece80decff5f9d401384fc9e734a4f380f3d
Mouzone <sunnyliu010@gmail.com>
2024-08-22 19:51:59 -0400

    Access genius api and print out the lyrics

--- lyrics.py
+++ lyrics.py
@@ -1,3 +1,14 @@
+from dotenv import load_dotenv
+from lyricsgenius import Genius
+
+import os
+
+load_dotenv()
+
+token = os.getenv('TOKEN')
+genius = Genius(token)
+song = genius.search_song("Build My Life", None)
+print(song.lyrics)
 # connect to genius api
 
 # take search terms
