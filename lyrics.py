906b18742f4317a769214c2a5c7d00cf35302fe4
Mouzone <sunnyliu010@gmail.com>
2024-08-22 20:04:05 -0400

    Split song lyrics by section

--- lyrics.py
+++ lyrics.py
@@ -1,22 +1,22 @@
 from dotenv import load_dotenv
 from lyricsgenius import Genius
-
 import os
 
-load_dotenv()
 
-token = os.getenv('TOKEN')
-genius = Genius(token)
-song = genius.search_song("Build My Life", None)
-print(song.lyrics)
-# connect to genius api
+def search(song_name, artist):
+    load_dotenv()
+    token = os.getenv('TOKEN')
+    genius = Genius(token)
+    song = genius.search_song(song_name, artist)
+    return song.lyrics
 
-# take search terms
 
-# query
+def main():
+    song_name = input("Enter song name: ")
+    artist = input("Enter artist name: ")
+    lyrics = search(song_name, artist)
+    lyrics_by_sections = lyrics.split("\n\n")
+    print(lyrics_by_sections)
 
-# look at results
 
-# select the best
-
-# return the lyrics of the best
+main()
