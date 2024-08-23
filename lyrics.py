88f254e94282546aa887b3b721b87b6ebd76dbcb
Mouzone <sunnyliu010@gmail.com>
2024-08-22 20:31:46 -0400

    Remove header and numeric+Embed at the end

--- lyrics.py
+++ lyrics.py
@@ -1,5 +1,7 @@
-from dotenv import load_dotenv
+from requests.exceptions import HTTPError, Timeout
 from lyricsgenius import Genius
+from dotenv import load_dotenv
+import re
 import os
 
 
@@ -7,16 +9,43 @@ def search(song_name, artist):
     load_dotenv()
     token = os.getenv('TOKEN')
     genius = Genius(token)
+    genius.excluded_terms = ["(Remix)", "(Live)"]
+
     song = genius.search_song(song_name, artist)
     return song.lyrics
+
+
+def clean(lyrics):
+    lyrics_no_header = lyrics.split("Lyrics")[1]
+    pattern = r'\d*Embed$'
+    lyrics_no_ending = re.sub(pattern, '', lyrics_no_header)
+    # add new line before every [ if there is no new line
+    print(lyrics_no_ending)
+
+    lyrics_list = lyrics_no_ending.split("\n\n")
+    lyrics_by_section = {}
+    for section in lyrics_list:
+        header_end = section.find('\n')
+        # Split the text into header and body
+        section_name = section[:header_end].strip()
+        body = section[header_end:].strip()
+        lyrics_by_section[section_name] = body
+
+    return lyrics_by_section
 
 
 def main():
     song_name = input("Enter song name: ")
     artist = input("Enter artist name: ")
-    lyrics = search(song_name, artist)
-    lyrics_by_sections = lyrics.split("\n\n")
-    print(lyrics_by_sections)
-
+    try:
+        lyrics = search(song_name, artist)
+        lyrics_by_sections = clean(lyrics)
+        print(lyrics_by_sections)
+    except HTTPError as e:
+        print(e.errno)
+        print(e.args[0])
+        print(e.args[1])
+    except Timeout:
+        pass
 
 main()
