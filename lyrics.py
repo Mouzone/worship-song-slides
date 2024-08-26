aa923d9ada9b8c14f5ba5a1c87d192115492ff4d
Mouzone <sunnyliu010@gmail.com>
2024-08-26 18:14:02 -0400

    Remove clean() function and add blank lines for readability

--- lyrics.py
+++ lyrics.py
@@ -28,23 +28,21 @@ def cleanNewlines(lyrics):
 def createLyricsDict(lyrics):
     lyrics_list = lyrics.split("\n\n")
     lyrics_list = lyrics_list[1:]
+
     lyrics_by_section = {}
     order = []
+
     for section in lyrics_list:
         header_end = section.find('\n')
         # Split the text into header and body
         if header_end != -1:
             section_name = section[:header_end].strip()
             body = section[header_end:].strip()
+
             order.append(section_name)
             lyrics_by_section[section_name] = body
 
     return order, lyrics_by_section
-
-
-def clean(lyrics):
-    lyrics_clean = cleanNewlines(lyrics)
-    return createLyricsDict(lyrics_clean)
 
 
 def main():
@@ -52,7 +50,8 @@ def main():
     artist = input("Enter artist name: ")
     try:
         lyrics = search(song_name, artist)
-        order, lyrics_by_sections = clean(lyrics)
+        lyrics_clean = cleanNewlines(lyrics)
+        order, lyrics_by_sections = createLyricsDict(lyrics_clean)
         print(order)
         print(lyrics_by_sections)
     except HTTPError as e:
