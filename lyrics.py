4ca06ba48e5631b5cb5b4a239c2475f288f6231d
Mouzone <sunnyliu010@gmail.com>
2024-08-26 18:08:57 -0400

    Store the order of each section appearing, and body and heading for each section with proper formatting

--- lyrics.py
+++ lyrics.py
@@ -14,32 +14,32 @@ def search(song_name, artist):
     song = genius.search_song(song_name, artist)
     return song.lyrics
 
-# todo: figure out why second [Verse 2] does not properly add \n before
-# -- "You might also like[Verse 2]"
-# remove all \n and \n\n before [ then replace [ with \n\n[ for proper
+
 def cleanNewlines(lyrics):
     lyrics_no_header = lyrics.split("Lyrics")[1]
-    pattern = r'\d*Embed$'
-    lyrics_no_ending = re.sub(pattern, '', lyrics_no_header)
-    lyrics_no_newlines = re.sub(r"\n\n", r'\n', lyrics_no_ending)
-    lyrics_space_between = re.sub(r"\[", r"\n[", lyrics_no_newlines)
-    lyrics_proper = re.sub(r"\n", "", lyrics_space_between, count=1)
-    # add new line before every [ if there is no new line
+
+    lyrics_no_ending = re.sub(r'\d*Embed$', '', lyrics_no_header)
+    lyrics_no_newlines = re.sub(r"\n\n", '', lyrics_no_ending)
+    lyrics_no_newlines = re.sub(r"\n\[", '[', lyrics_no_newlines)
+    lyrics_proper = re.sub(r"\[", "\n\n[", lyrics_no_newlines)
     return lyrics_proper
 
 
-# todo: store order somehow since verse 2 repeats
 def createLyricsDict(lyrics):
     lyrics_list = lyrics.split("\n\n")
+    lyrics_list = lyrics_list[1:]
     lyrics_by_section = {}
+    order = []
     for section in lyrics_list:
         header_end = section.find('\n')
         # Split the text into header and body
-        section_name = section[:header_end].strip()
-        body = section[header_end:].strip()
-        lyrics_by_section[section_name] = body
+        if header_end != -1:
+            section_name = section[:header_end].strip()
+            body = section[header_end:].strip()
+            order.append(section_name)
+            lyrics_by_section[section_name] = body
 
-    return lyrics_by_section
+    return order, lyrics_by_section
 
 
 def clean(lyrics):
@@ -52,7 +52,8 @@ def main():
     artist = input("Enter artist name: ")
     try:
         lyrics = search(song_name, artist)
-        lyrics_by_sections = clean(lyrics)
+        order, lyrics_by_sections = clean(lyrics)
+        print(order)
         print(lyrics_by_sections)
     except HTTPError as e:
         print(e.errno)
