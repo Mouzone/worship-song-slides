f3d642190f2af7158e46ebafe4a890a875c23077
Mouzone <sunnyliu010@gmail.com>
2024-08-26 20:16:34 -0400

    Change font to white and background to black for slides

--- slides.py
+++ slides.py
@@ -2,13 +2,14 @@ from pptx import Presentation
 from pptx.util import Inches, Pt
 from pptx.enum.text import PP_ALIGN
 from pptx.enum.text import MSO_ANCHOR
+from pptx.dml.color import RGBColor
 import lyrics
 
 # search for song, then get order and dictionary
 # create slide
-# -- one textbox
-# -- center align all text
-# -- use theme (background image)
+# -- some kind of gui to modify text content
+# ---- modify font size
+# ---- create slides and delete slides as well
 
 song_name = input("Enter song name: ")
 artist = input("Enter artist name: ")
@@ -21,13 +22,25 @@ left = (ppt.slide_width - width) / 2
 top = (ppt.slide_height - height) / 2
 for section_name in order:
     slide = ppt.slides.add_slide(blank_slide_layout)
+
+    background = slide.background
+    fill = background.fill
+    fill.solid()
+    fill.fore_color.rgb = RGBColor(0, 0, 0)
+
     text_box = slide.shapes.add_textbox(left, top, width, height)
     tf = text_box.text_frame
+
     p = tf.paragraphs[0]
-    p.text = lyrics_by_section[section_name]
     p.font.size = Pt(30)
     p.alignment = PP_ALIGN.CENTER
     tf.vertical_anchor = MSO_ANCHOR.MIDDLE
+
+    run = p.add_run()
+    run.text = lyrics_by_section[section_name]
+    font = run.font
+    font.color.rgb = RGBColor(0xFF, 0xFF, 0xFF)
+
 
 ppt.save(f"{song_name}.pptx")
 
